3f2f36eed4e3118dc06c16b9f8f204f6af1b3cb7
Yifan Xu <xuyifan02@pingcap.com>
2022-03-11 11:36:17 +0800

    add a new tidb module

--- filebeat/include/list.go
+++ filebeat/include/list.go
@@ -49,6 +49,7 @@ import (
 	_ "github.com/elastic/beats/v7/filebeat/module/redis"
 	_ "github.com/elastic/beats/v7/filebeat/module/santa"
 	_ "github.com/elastic/beats/v7/filebeat/module/system"
+	_ "github.com/elastic/beats/v7/filebeat/module/tidb"
 	_ "github.com/elastic/beats/v7/filebeat/module/tikv"
 	_ "github.com/elastic/beats/v7/filebeat/module/traefik"
 )
